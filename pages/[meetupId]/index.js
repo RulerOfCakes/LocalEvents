dff3ca07f91cf9c04c56203fd1728640688beafd
RulerOfCakes <fragmentbombs@gmail.com>
2021-09-03 01:14:06 +0900

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -56,7 +56,7 @@ export async function getStaticPaths() {
   const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
   client.close();
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => {
       return {
         params: {
